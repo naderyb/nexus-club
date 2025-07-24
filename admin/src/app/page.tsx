c82788bd5b79e05b62f323303d8e062d75f49e88
youb mahmoud nader <163655137+naderyb@users.noreply.github.com>
2025-07-24 22:15:14 +0100

    Delete admin directory

--- admin/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl mb-6 text-glow">Welcome to Nexus Club</h1>
-      <p>For admins, please go to the <a href="/nx-admin" className="text-blue-500">Login Page</a></p>
-    </div>
-  )
-}
