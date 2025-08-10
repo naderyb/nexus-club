029d48730af30cb5d931b80fa592a4178948bb59
Nader youb <youb.nader@gmail.com>
2025-08-11 00:36:53 +0100

    feat: add mediaItems data for image assets

---
+++ src/app/data/mediaItems.ts
@@ -0,0 +1,8 @@
+export const mediaItems = [
+  { type: "image", src: "/media/groupe2-itfc.jpeg", alt: "" },
+  { type: "image", src: "/media/heetch.jpeg", alt: "" },
+  { type: "image", src: "/media/heetch3.jpeg", alt: "" },
+  { type: "image", src: "/media/2nd_expo-itfc.jpeg", alt: "" },
+  { type: "image", src: "/media/workshop1.jpeg", alt: "" },
+  { type: "image", src: "/media/tshirt-day.jpeg", alt: "" },
+] as const;
